2c0aa714ab4f692adabcbe0e38856606286b820c
Agarwal Vishal <agarwal.vishal@snapdeal.com>
2020-11-06 20:40:53 +0530

    Added code for reversing sentence keeping space

---
+++ src/main/java/com/nokia/dev/string/problems/ReverseMaintainingSpace.java
@@ -0,0 +1,21 @@
+package com.nokia.dev.string.problems;
+
+public class ReverseMaintainingSpace {
+
+	public static void main(String[] args) {
+		String str = "java and automation" ;
+		String outputStr = "" ;
+		int count = 0 ;
+		
+		for(int i = str.length() - 1 ; i >= 0 ; i--) {
+			if(str.charAt(count) == ' ') {
+				outputStr = outputStr + str.charAt(count) ;
+			} else if(str.charAt(i) != ' ') {
+				outputStr = outputStr + str.charAt(i) ;				
+			}
+			count++ ;
+		}
+		
+		System.out.println(outputStr);
+	}
+}
