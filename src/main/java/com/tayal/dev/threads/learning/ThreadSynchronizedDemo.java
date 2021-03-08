99c09b07cc1c9c38bdbdef4cad8d1e0a57030b9a
Vishal Agarwal <vishal.agarwal@zupee.in>
2021-03-08 22:29:32 +0530

    Added synchronized block programs

--- src/main/java/com/tayal/dev/threads/learning/ThreadSynchronizedDemo.java
+++ src/main/java/com/tayal/dev/threads/learning/ThreadSynchronizedDemo.java
@@ -2,6 +2,14 @@ package com.tayal.dev.threads.learning;
 
 class Display {
 		public synchronized void wish(String name) {
+		/*
+		 * If a thread wants to execute a static synchronized method then
+		 * thread require class level lock. Once thread got class level 
+		 * lock then it is allowed to execute any static synchronized method of
+		 * that class. Once method execution completes, automatically, thread
+		 * releases the lock.
+		 */
+//		public static synchronized void wish(String name) { // 
 			try {
 				for(int i = 0 ; i < 5 ; i++) {
 					System.out.print("Good Morning : ");
@@ -34,9 +42,12 @@ class ThreadSynchronizedDemo {
 
 	public static void main(String[] args) {
 		Display d1 = new Display();
+//		Display d2 = new Display();	// Multiple threads operating on multiple objects 
+									// then there is no impact of synchronization
 		
 		MyThreadSynchronizedMethod r1 = new MyThreadSynchronizedMethod(d1, "Vishal");
 		MyThreadSynchronizedMethod r2 = new MyThreadSynchronizedMethod(d1, "Naina");
+//		MyThreadSynchronizedMethod r2 = new MyThreadSynchronizedMethod(d2, "Naina");
 		Thread t1 = new Thread(r1);
 		Thread t2 = new Thread(r2);
 		
