4cc4d93fc7f7b40729c4f0c15f339716f22e88cd
Vishal Agarwal <vishal.agarwal@zupee.in>
2021-10-17 17:52:45 +0530

    Changed comment

--- src/main/java/com/learning/designpatterns/DesignPattern.java
+++ src/main/java/com/learning/designpatterns/DesignPattern.java
@@ -17,12 +17,12 @@ public class DesignPattern {
 		// User 2
 		System.out.println("User 2 Accessing Cloud resource for second time");
 		SingletonClass singletonClass2 = SingletonClass.getSharedInstance();
-		System.out.println("Hashcode for shared cloud user 1 : " + singletonClass2.hashCode());
+		System.out.println("Hashcode for shared cloud user 2 : " + singletonClass2.hashCode());
 				
 		// User 3
 		System.out.println("User 3 Accessing Cloud resource for third time");
 		SingletonClass singletonClass3 = SingletonClass.getSharedInstance();
-		System.out.println("Hashcode for shared cloud user 1 : " + singletonClass3.hashCode());
+		System.out.println("Hashcode for shared cloud user 3 : " + singletonClass3.hashCode());
 	}
 
 }
